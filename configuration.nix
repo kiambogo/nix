28b7ddfb388cf180893a20b7365fd6f3b78b7fb1
Christopher Poenaru <kiambogo@gmail.com>
2020-12-25 23:03:54 +0000

    xresource, caps->esc

--- configuration.nix
+++ configuration.nix
@@ -69,15 +69,64 @@
         enable = false;
 	twoFingerScroll = true;
       };
+      xautolock = {
+        enable = true;
+        time = 10; # mins
+        locker = "${pkgs.xlockmore}/bin/xlock -mode space";
+      };
+      xkbOptions = "caps:escape";
     };
     devmon.enable = true;
+    autorandr = {
+      enable = true;
+    };
   };
 
 services.xserver.displayManager.sessionCommands = ''
    ${pkgs.xorg.xrdb}/bin/xrdb -merge <<EOF
       Xft.dpi: 144
-      Xcursor.theme: Adwaita
-      Xcursor.size: 64
+      Xcursor.theme: xcursor-breeze
+      Xcursor.size: 0
+      Xft.antialias: true
+      Xft.hinting:   true
+      Xft.rgba:      rgb
+      Xft.autohint:  false
+      Xft.hintstyle: hintslight
+      Xft.lcdfilter: lcddefault
+
+      !! Theme
+      *background:                      #222D31
+      *foreground:                      #d8d8d8
+      *fading:                          8
+      *fadeColor:                       black
+      *cursorColor:                     #1ABB9B
+      *pointerColorBackground:          #2B2C2B
+      *pointerColorForeground:          #16A085
+
+      !! black dark/light
+      *color0:                          #222D31
+      *color8:                          #585858
+      !! red dark/light
+      *color1:                          #ab4642
+      *color9:                          #ab4642
+      !! green dark/light
+      *color2:                          #7E807E
+      *color10:                         #8D8F8D
+      !! yellow dark/light
+      *color3:                          #f7ca88
+      *color11:                         #f7ca88
+      !! blue dark/light
+      *color4:                          #7cafc2
+      *color12:                         #7cafc2
+      !! magenta dark/light
+      *color5:                          #ba8baf
+      *color13:                         #ba8baf
+      !! cyan dark/light
+      *color6:                          #1ABB9B
+      *color14:                         #1ABB9B
+      !! white dark/light
+      *color7:                          #d8d8d8
+      *color15:                         #f8f8f8
    EOF
 '';
 
@@ -121,6 +170,7 @@ services.xserver.displayManager.sessionCommands = ''
     kitty
     xorg.xmodmap
     emacs
+    tmux
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
