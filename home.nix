bdc3e9c30a9409ae63d5be0edff2910661d9d603
Christopher Poenaru <kiambogo@gmail.com>
2020-12-26 03:37:38 +0000

    kitty

--- home.nix
+++ home.nix
@@ -36,6 +36,26 @@
     aliases = { st = "status"; lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"; };
     extraConfig = { core = { editor = "vim"; }; color = { ui = true; }; pull = { rebase = true; }; };
   };
+  kitty = {
+    enable = true;
+    font = {
+      package = pkgs.fira-code;
+      name = "Fira Code 11";
+    };
+    keybindings = {
+      "cmd+equal" = "change_font_size all +2.0";
+      "cmd+minus" = "change_font_size all -2.0";
+      "cmd+backspace" = "change_font_size all 0";
+      "cmd+c" = "copy_to_clipboard";
+      "cmd+v" = "paste_from_clipboard";
+    };
+    settings = {
+      scrollback_lines = 10000;
+      enable_audio_bell = "no";
+      window_padding_width = "3.0";
+      hide_window_decorations = "yes";
+    };
+  };
 };
 
   # This value determines the Home Manager release that your
