1e0bea7ee262daaac37f955d0816dac0bdcdac6b
Christopher Poenaru <kiambogo@gmail.com>
2020-12-27 02:27:12 +0000

    moar tmux conf

--- home.nix
+++ home.nix
@@ -9,6 +9,9 @@
   home.username = "christopher";
   home.homeDirectory = "/home/christopher";
 
+  services = {
+  };
+
  programs = {
    firefox = {
     enable = true;
@@ -90,6 +93,7 @@
       set-option -g renumber-windows on
       set-window-option -g automatic-rename on
       set-option -g set-titles on
+      set -sg escape-time 0
       bind-key v split-window -h
       bind-key s split-window -v
       bind-key -n C-S-Left swap-window -t -1
@@ -97,6 +101,14 @@
       set -g prefix C-a
       unbind C-b
       bind C-a send-prefix
+      bind -n C-h run "tmux select-pane -L"
+      bind -n C-j run "tmux select-pane -D"
+      bind -n C-k run "tmux select-pane -U"
+      bind -n C-l run "tmux select-pane -R"
+      unbind-key -T copy-mode-vi v
+      bind-key -T copy-mode-vi v send-keys -X begin-selection
+      bind-key -T copy-mode-vi 'C-v' send-keys -X rectangle-toggle
+      bind-key r source-file ~/.tmux.conf \; display "Config reloaded!"
     '';
   };
 };
